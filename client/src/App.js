446d0bdf2d86b6e2e1f2e748540a0cb728925f13
Zack <reportzack@gmail.com>
2024-04-29 08:30:01 -0500

    feat: Initial structure for Store Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
 import { UserCreateComponent } from "./components/users/UserCreateComponent";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers";
+
+const store = configureStore({
+    reducer: rootReducer,
+});
 
 function App() {
-    return <UserCreateComponent />;
+    return (
+        <Provider store={store}>
+            <UserCreateComponent />
+        </Provider>
+    );
 }
 
 export default App;
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,1 +1,12 @@
 export * from "./user";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Provider store={store}>
+        <UserCreateComponent />
+    </Provider>
+);
