7f8b58b942c24189003a31111059eaa1ca23d215
Zack <reportzack@gmail.com>
2024-05-23 19:10:04 -0500

    feat: vehicle slice created

--- client/src/slices/store.js
+++ client/src/slices/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import vehicleReducer from "./vehicleSlice";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        vehicle: vehicleReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
