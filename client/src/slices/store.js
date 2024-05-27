926713e85b3f9b136fd70ec60a1a65777529dcdb
Zack <reportzack@gmail.com>
2024-05-26 21:24:44 -0500

    feat: tasks reducer added to the store provider

--- client/src/slices/store.js
+++ client/src/slices/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import vehicleReducer from "./vehicleSlice";
+import taskReducer from "./taskSlice";
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         vehicle: vehicleReducer,
+        task: taskReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
