ad721514f86b481dd3172e18ead1e69dc77d74c1
Zack <reportzack@gmail.com>
2024-06-10 17:34:08 -0500

    feat: Add PneumaticRoutes for CRUD operations on pneumatics

---
+++ server/routes/PneumaticRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const PneumaticController = require("../controller/PneumaticController");
+const multer = require("multer");
+
+const router = express.Router();
+const upload = multer({});
+
+// Get all pneumatics
+router.get("/", PneumaticController.getPneumatics);
+
+// Get a single pneumatic by ID
+router.get("/:id", PneumaticController.getPneumaticById);
+
+// Create a new pneumatic
+router.post("/new", upload.none(), PneumaticController.createPneumatic);
+
+// Update a pneumatic by ID
+router.patch("/edit/:id", upload.none(), PneumaticController.updatePneumatic);
+
+// Delete a pneumatic by ID
+router.delete("/delete/:id", PneumaticController.deletePneumatic);
+
+module.exports = router;
