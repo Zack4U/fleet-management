886815b991c7264a6ed59093065730b8926d2d74
Zack <reportzack@gmail.com>
2024-06-16 18:30:21 -0500

    feature: getDrivers endpoint to UserController.js and add position field to createVehicle endpoint in VehicleController.js

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -233,6 +233,20 @@ const signUp = async (req, res) => {
     }
 };
 
+const getDrivers = async (req, res) => {
+    try {
+        const drivers = await prisma.user.findMany({
+            where: {
+                role: "DRIVER",
+            },
+        });
+        res.status(201).json(drivers);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Failed to get drivers" });
+    }
+};
+
 module.exports = {
     getUsers,
     getUserById,
@@ -243,4 +257,5 @@ module.exports = {
     login,
     logout,
     signUp,
+    getDrivers,
 };
