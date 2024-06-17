1872209379195bc415dff8e2c9b05b5d8b355d9e
Zack <reportzack@gmail.com>
2024-06-16 21:46:24 -0500

    feat: Basic Pages for Driver componentes

---
+++ client/src/pages/DriverPages/routes/MyRouteListComponent.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyRouteListComponent() {
+    return <div>MyRouteListComponent</div>;
+}
---
+++ client/src/pages/DriverPages/vehicles/MyVehicleListComponent.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyVehicleListComponent() {
+    return <div>MyVehicleListComponent</div>;
+}
