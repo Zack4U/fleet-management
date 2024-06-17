15e71248cb79f9edb32db5031d7add2483fb51ba
Zack <reportzack@gmail.com>
2024-06-16 21:47:28 -0500

    Refactor App.js to include DriverRoutes based on user role

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import { BrowserRouter } from "react-router-dom";
 import { AdminRoutes } from "./routes/AdminRoutes";
 import { GeneralRoutes } from "./routes/GeneralRoutes";
 import { ThemeProvider } from "./ThemeContext";
+import { DriverRoutes } from "./routes/DriverRoutes";
+import { useContext, useEffect, useState } from "react";
+import UserContext from "./utils/userContext";
 
 function App() {
+    const { role } = useContext(UserContext);
+
     return (
         <ThemeProvider>
             <BrowserRouter>
-                <AdminRoutes />
-                <GeneralRoutes />
+                {role === "DRIVER" && <DriverRoutes />}
+                {role === "ADMIN" && <AdminRoutes />}
+                {role === null && <GeneralRoutes />}
             </BrowserRouter>
         </ThemeProvider>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import { store } from "./slices/store";
 import { Provider } from "react-redux";
 import App from "./App";
+import { UserProvider } from "./utils/userContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Provider store={store}>
-        <App />
+        <UserProvider>
+            <App />
+        </UserProvider>
     </Provider>
 );
