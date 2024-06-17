4dcfe92e74976223295e6eaf1692146e26e3637a
Zack <reportzack@gmail.com>
2024-06-16 21:48:00 -0500

    Refactor AdminRoutes.js to include PageNotFoundComponent for unknown routes

--- client/src/routes/AdminRoutes.js
+++ client/src/routes/AdminRoutes.js
@@ -8,16 +8,16 @@ import { VehicleCreateComponent } from "../pages/AdminPages/vehicles/VehicleCrea
 import { DragNDrop } from "../pages/AdminPages/tasks/DragNDrop";
 import { AdminDashboardComponent } from "../pages/AdminPages/admin/AdminDashboardComponent";
 import { ToastContainer, toast } from "react-toastify";
+import PageNotFoundComponent from "../components/error/PageNotFoundComponent";
 import "react-toastify/dist/ReactToastify.css";
 
 export const AdminRoutes = () => {
     const token = localStorage.getItem("token");
     const role = localStorage.getItem("role");
     const navigate = useNavigate();
-    console.log(token, role);
 
     useEffect(() => {
-        if (token == null || role !== "admin") {
+        if (token == null || role !== "ADMIN") {
             toast.error("You are not authorized to view this page!");
             navigate("/login");
         }
@@ -59,6 +59,7 @@ export const AdminRoutes = () => {
                     path="/admin/tasks"
                     element={loadLayout(AdminLayout, DragNDrop)}
                 />
+                <Route path="*" element={<PageNotFoundComponent />} />
             </Routes>
         </>
     );
